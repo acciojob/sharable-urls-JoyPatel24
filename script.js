6ef071c2e6bed90be95f3f2fdcda45a148449d3d
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2024-02-03 10:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+function updateURL() {
+        // Get values from input fields
+        var nameValue = document.getElementById("name").value;
+        var yearValue = document.getElementById("year").value;
+
+        // Update the h3 element with the generated URL
+        var urlElement = document.getElementById("url");
+        urlElement.textContent = "https://localhost:8080/?name=" + encodeURIComponent(nameValue) + "&year=" + encodeURIComponent(yearValue);
+    }
